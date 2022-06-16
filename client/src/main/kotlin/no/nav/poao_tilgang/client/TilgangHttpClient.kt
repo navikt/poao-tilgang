dd333c038def9ec071aa3c8b05e34c69bbf035f2
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-06-16 10:52:00 +0200

    Implementer TilgangHttpClient

--- client/src/main/kotlin/no/nav/poao_tilgang/client/TilgangHttpClient.kt
+++ client/src/main/kotlin/no/nav/poao_tilgang/client/TilgangHttpClient.kt
@@ -1,12 +1,38 @@
 package no.nav.poao_tilgang.client
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.common.rest.client.RestClient
+import no.nav.common.utils.UrlUtils.joinPaths
 import no.nav.poao_tilgang.api.dto.HarTilgangTilModiaRequest
+import no.nav.poao_tilgang.client.KotlinObjectMapper.objectMapper
 import no.nav.poao_tilgang.core.domain.Decision
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
-class TilgangHttpClient : TilgangClient {
+class TilgangHttpClient(val baseUrl: String, val authToken: () -> String) : TilgangClient {
+
+	private val client: OkHttpClient = RestClient.baseClient()
+
 	override fun harVeilederTilgangTilModia(navIdent: String): Decision {
-//		val requestJson = toJsonString(HarTilgangTilModiaRequest(navIdent))
+		val requestJson = objectMapper.writeValueAsString(HarTilgangTilModiaRequest(navIdent))
 
-		TODO("Not yet implemented")
+
+		val request = Request.Builder()
+			.url(joinPaths(baseUrl, "/api/v1/tilgang/modia"))
+			.post(requestJson.toRequestBody("application/json".toMediaType()))
+			.header("Authorization", "Bearer ${authToken()}")
+			.build()
+
+		return client.newCall(request).execute().let { response ->
+			if (!response.isSuccessful) {
+				throw RuntimeException("Feilende kall med statuskode ${response.code} mot $baseUrl")
+			}
+
+			val body = response.body?.string() ?: throw RuntimeException("Body is missing")
+
+			objectMapper.readValue(body)
+		}
 	}
 }
