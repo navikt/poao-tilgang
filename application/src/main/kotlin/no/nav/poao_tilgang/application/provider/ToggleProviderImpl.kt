cb0dfc2e9ccd15a8de589a6d545a251d637897ba
haakonph <hakon.monn-holm@nav.no>
2023-05-31 14:02:24 +0200

    toggel egen mot aback response

---
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/provider/ToggleProviderImpl.kt
@@ -0,0 +1,10 @@
+package no.nav.poao_tilgang.application.provider
+
+import no.nav.common.featuretoggle.UnleashClient
+import no.nav.poao_tilgang.core.provider.ToggleProvider
+
+class ToggleProviderImpl(private val unleashClient: UnleashClient) : ToggleProvider {
+	override fun brukAbacDesision(): Boolean {
+		return !unleashClient.isEnabled("poao-tilgang.use-poao-tilgang-decision")
+	}
+}
