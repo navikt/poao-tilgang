cd2bd2cc09ef2dc2a74686c8a700257701af6e47
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-04-11 14:35:10 +0200

    Skift ut pdl client med pdlpip klient der den er brukt

--- application/src/main/kotlin/no/nav/poao_tilgang/application/provider/DiskresjonskodeProviderImpl.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/provider/DiskresjonskodeProviderImpl.kt
@@ -1,26 +1,28 @@
 package no.nav.poao_tilgang.application.provider
 
-import no.nav.poao_tilgang.application.client.pdl.Adressebeskyttelse
-import no.nav.poao_tilgang.application.client.pdl.PdlClient
+import no.nav.poao_tilgang.application.client.pdl_pip.Adressebeskyttelse
+import no.nav.poao_tilgang.application.client.pdl_pip.Gradering
+import no.nav.poao_tilgang.application.client.pdl_pip.PdlPipClient
 import no.nav.poao_tilgang.core.domain.Diskresjonskode
 import no.nav.poao_tilgang.core.provider.DiskresjonskodeProvider
 import org.springframework.stereotype.Service
 
 @Service
 class DiskresjonskodeProviderImpl(
-	private val pdlClient: PdlClient
+	private val pdlPipClient: PdlPipClient
 ) : DiskresjonskodeProvider {
 
 	override fun hentDiskresjonskode(norskIdent: String): Diskresjonskode? {
-		return pdlClient.hentBrukerInfo(norskIdent)
-			.adressebeskyttelse?.let { tilDiskresjonskode(it) }
+		return pdlPipClient.hentBrukerInfo(norskIdent)
+			?.person
+			?.adressebeskyttelse?.firstOrNull()?.let { tilDiskresjonskode(it) }
 	}
 
 	private fun tilDiskresjonskode(adressebeskyttelse: Adressebeskyttelse): Diskresjonskode {
-		return when(adressebeskyttelse) {
-			Adressebeskyttelse.FORTROLIG -> Diskresjonskode.FORTROLIG
-			Adressebeskyttelse.STRENGT_FORTROLIG -> Diskresjonskode.STRENGT_FORTROLIG
-			Adressebeskyttelse.STRENGT_FORTROLIG_UTLAND -> Diskresjonskode.STRENGT_FORTROLIG_UTLAND
+		return when(adressebeskyttelse.gradering) {
+			Gradering.FORTROLIG -> Diskresjonskode.FORTROLIG
+			Gradering.STRENGT_FORTROLIG -> Diskresjonskode.STRENGT_FORTROLIG
+			Gradering.STRENGT_FORTROLIG_UTLAND -> Diskresjonskode.STRENGT_FORTROLIG_UTLAND
 		}
 	}
 
