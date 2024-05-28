9a1335ffa5e8169866cbf8dedae729fd63dc866e
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-05-28 11:59:14 +0200

    Fjerne enda noen flere logger

--- application/src/main/kotlin/no/nav/poao_tilgang/application/provider/AdGruppeProviderImpl.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/provider/AdGruppeProviderImpl.kt
@@ -71,7 +71,7 @@ class AdGruppeProviderImpl(
 
 	override fun hentAdGrupper(navAnsattAzureId: AzureObjectId): List<AdGruppe> {
 		return hentAdGrupperForNavAnsattWithCache(navAnsattAzureId).also {
-			secureLog.info("Ms graph hentAdGrupper navAnsattAzureId: $navAnsattAzureId result: $it")
+		//	secureLog.info("Ms graph hentAdGrupper navAnsattAzureId: $navAnsattAzureId result: $it")
 		}
 	}
 
@@ -93,7 +93,7 @@ class AdGruppeProviderImpl(
 			navIdentToAzureIdCache.put(navIdent, navAnsattAzureId)
 			navIdent
 		}.also {
-			secureLog.info("MS Graph hentNavIdent navAnsattAzureId: $navAnsattAzureId result: $it")
+		//	secureLog.info("MS Graph hentNavIdent navAnsattAzureId: $navAnsattAzureId result: $it")
 		}
 	}
 
