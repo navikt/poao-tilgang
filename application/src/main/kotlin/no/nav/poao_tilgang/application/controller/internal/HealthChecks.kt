ac0cc1b69c81b34946bdd513e483c10a571e6f41
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-10-03 18:39:29 +0200

    Fjern abac ping fra helsesjekk

--- application/src/main/kotlin/no/nav/poao_tilgang/application/controller/internal/HealthChecks.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/controller/internal/HealthChecks.kt
@@ -1,6 +1,7 @@
 package no.nav.poao_tilgang.application.controller.internal
 
 import no.nav.common.abac.AbacClient
+import no.nav.poao_tilgang.core.provider.ToggleProvider
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
@@ -9,16 +10,22 @@ import org.springframework.stereotype.Component
 
 @Component
 class HealthChecksPoaoTilgang(
-	private val abacClient: AbacClient
+	private val abacClient: AbacClient,
 ): HealthIndicator {
 	private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 	override fun health(): Health {
-		logger.debug("Starter healthcheck for abacClient")
-		if (abacClient.checkHealth().isHealthy){
-			logger.debug("Healthcheck for abacClient: READY")
-			return Health.up().build()
-		}
-		logger.warn("Healthcheck for abacClient: NOT READY")
-		return Health.down().build()
+		/*
+		Vi har avhengigheter til:
+		* Axsys
+		* Microsoft graph api
+		* Norg
+		* Pdl
+		* Skjermet Person
+		* Veilarbarena
+		Men de ulike policyene har forskjellige avhengigheter.
+		TODO Vurder om vi skal ha noen eller alle avhengigheter inn i healthcheck.
+		NB Ingen av klientene våre har innebygget støtte for ping, så det må i så fall implementeres.
+		 */
+		return Health.up().build()
 	}
 }
