04f0fa0d06a5d271e9c4656422f7402a73374a91
Sigurd Grøneng <sigurdgroneng@gmail.com>
2025-03-17 15:12:22 +0100

    Nake navstatus endpoint @Unprotected (#281)

--- application/src/main/kotlin/no/nav/poao_tilgang/application/controller/internal/NavStatusController.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/controller/internal/NavStatusController.kt
@@ -1,5 +1,6 @@
 package no.nav.poao_tilgang.application.controller.internal
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class NavStatusController(
 ) {
 
+	@Unprotected
 	@GetMapping
 	fun okStatus(): NavStatusDto {
 
